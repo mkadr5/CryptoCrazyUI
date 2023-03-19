52441d618a5e0cd9578839e2c68666292176cdc4
mkadr5 <muhammetkadir52@gmail.com>
2023-03-19 14:43:22 +0300

    Initial Commit

---
+++ CryptoCrazyUI/CryptoCrazyUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCrazyUIApp.swift
+//  CryptoCrazyUI
+//
+//  Created by Muhammet Kadir on 19.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCrazyUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
